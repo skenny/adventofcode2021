64397f3f3f8002f96d62d9a1f1aa42c129711c68
Shaun Kenny <skenny@gmail.com>
2021-12-02 21:46:53 -0400

    day 2

---
+++ day2.rs
@@ -0,0 +1,77 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
+pub fn run() {
+    println!("AOC 2021 - Day 2");
+
+    //let sample_input = read_input("input/day2-sample.txt");
+    //let real_input = read_input("input/day2.txt");
+
+    println!("sample 1 = {}", part1(read_input("input/day2-sample.txt")));
+    println!("part 1 = {}", part1(read_input("input/day2.txt")));
+
+    println!("sample 2 = {}", part2(read_input("input/day2-sample.txt")));
+    println!("part 2 = {}", part2(read_input("input/day2.txt")));
+}
+
+fn part1(input: Vec<String>) -> i32 {
+    let mut x = 0;
+    let mut depth = 0;
+
+    for command in input {
+        let parts: Vec<&str> = command.split_whitespace().collect();
+        let action: &str = &parts.get(0).unwrap();
+        let distance: &i32 = &parts.get(1).unwrap().parse().unwrap();
+
+        match action {
+            "forward" => x += distance,
+            "up" => depth -= distance,
+            "down" => depth += distance,
+            &_ => ()
+        }
+    }
+
+    //println!("x={}, depth={}", x, depth);
+
+    x * depth
+}
+
+fn part2(input: Vec<String>) -> i32 {
+    let mut x = 0;
+    let mut aim = 0;
+    let mut depth = 0;
+
+    for command in input {
+        let parts: Vec<&str> = command.split_whitespace().collect();
+        let action: &str = &parts.get(0).unwrap();
+        let distance: &i32 = &parts.get(1).unwrap().parse().unwrap();
+
+        match action {
+            "forward" => { x += distance; depth += aim * distance; },
+            "up" => aim -= distance,
+            "down" => aim += distance,
+            &_ => ()
+        }
+
+        //println!("x={}, aim={}, depth={}", x, aim, depth);
+    }
+
+    //println!("x={}, aim={}, depth={}", x, aim, depth);
+
+    x * depth
+}
+
+fn read_input(filename: &str) -> Vec<String> {
+    let mut v = Vec::new();
+    let file = File::open(filename).expect("cannot find file!");
+    let reader = BufReader::new(file);
+
+    // very inefficient, creates a new String for each line in the file
+    // tutorial has better examples of how to read lines from a file more efficiently
+    for line in reader.lines() {
+        v.push(line.unwrap());
+    }
+
+    v
+}
