be354bbe796de183e151e81cbc6f8da053b9fe8f
Shaun Kenny <skenny@gmail.com>
2021-12-13 17:05:29 -0400

    fuckin day 12 part 2

--- day12.rs
+++ day12.rs
@@ -6,57 +6,66 @@ pub fn run() {
 
     let sample_input_1 = aoc::read_input("input/day12-sample-1.txt");
     println!("sample 1 1 = {}", part1(&sample_input_1));
-    //println!("sample 1 2 = {}", part2(&sample_input_1));
+    println!("sample 1 2 = {}", part2(&sample_input_1));
 
     let sample_input_2 = aoc::read_input("input/day12-sample-2.txt");
     println!("sample 2 1 = {}", part1(&sample_input_2));
+    println!("sample 2 2 = {}", part2(&sample_input_2));
 
     let sample_input_3 = aoc::read_input("input/day12-sample-3.txt");
     println!("sample 3 1 = {}", part1(&sample_input_3));
+    println!("sample 3 2 = {}", part2(&sample_input_3));
 
     let real_input = aoc::read_input("input/day12.txt");
     println!("part 1 = {}", part1(&real_input));
-    //println!("part 2 = {}", part2(&real_input));
+    println!("part 2 = {}", part2(&real_input));
 }
 
 fn part1(input: &[String]) -> usize {
     let caves = parse_input(input);
-    let paths = explore_caves(&caves, "start");
+    let paths = explore_caves(&caves, "start", 1);
     paths.len()
 }
 
 fn part2(input: &[String]) -> usize {
-    0
+    let caves = parse_input(input);
+    let paths = explore_caves(&caves, "start", 2);
+    paths.len()
 }
 
-fn explore_caves(caves: &HashMap<String, Cave>, from_cave: &str) -> Vec<Vec<String>> {
-    explore_caves2(caves, from_cave, &mut Vec::new())
+fn explore_caves(caves: &HashMap<String, Cave>, from_cave: &str, max_small_visits: usize) -> Vec<Vec<String>> {
+    explore_caves2(caves, from_cave, &mut Vec::new(), max_small_visits, 0)
 }
 
-fn explore_caves2(caves: &HashMap<String, Cave>, start_cave: &str, current_path: &mut Vec<String>) -> Vec<Vec<String>> {
+fn explore_caves2(caves: &HashMap<String, Cave>, current_cave_name: &str, current_path: &mut Vec<String>, max_small_visits: usize, indent: usize) -> Vec<Vec<String>> {
     let mut paths: Vec<Vec<String>> = Vec::new();
 
-    current_path.push(start_cave.to_string());
+    let current_cave = &caves[current_cave_name];
+    current_path.push(current_cave_name.to_string());
 
-    //println!("exploring from {:?}", current_path);
-
-    if start_cave == "end" {
-        //println!("path: {:?}", current_path);
+    if current_cave_name == "end" {
         paths.push(current_path.to_vec());
     } else {
-        for connected_cave in (&caves[start_cave]).connected_caves.iter() {
-            if caves[connected_cave].is_big() || !current_path.contains(connected_cave) {
-                //println!("checking: {}", connected_cave);
-                for path in explore_caves2(caves, connected_cave, current_path) {
-                    paths.push(path.to_vec());
+        for connected_cave in current_cave.connected_caves.iter() {
+            if connected_cave != "start" {
+                let connected_cave_visits = if caves[connected_cave].is_big() { 0 } else { count_visits(current_path, connected_cave) };
+                if connected_cave_visits < max_small_visits {
+                    let new_max_small_visits = if connected_cave_visits + 1 == max_small_visits { 1 } else { max_small_visits };
+                    for path in explore_caves2(caves, connected_cave, current_path, new_max_small_visits, indent+1) {
+                        paths.push(path.to_vec());
+                    }
                 }
             }
         }
     }
 
-    current_path.pop();        
+    current_path.pop();
 
     paths
+}
+
+fn count_visits(path: &Vec<String>, cave: &str) -> usize {
+    path.iter().filter(|&path_cave| path_cave == cave).count()
 }
 
 fn parse_input(input: &[String]) -> HashMap<String, Cave> {
