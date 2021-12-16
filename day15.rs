7eff937832bd3908f97671424f3a685b3b99acff
Shaun Kenny <skenny@gmail.com>
2021-12-15 21:50:18 -0400

    day 15 refactored to Cave struct

--- day15.rs
+++ day15.rs
@@ -2,8 +2,6 @@ use crate::aoc;
 
 const NEIGHBOURS: [(i8, i8); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
 
-static mut best_path: i64 = i64::MAX;
-
 pub fn run() {
     println!("AOC 2021 - Day 15");
 
@@ -16,86 +14,109 @@ pub fn run() {
     // println!("part 2 = {}", part2(&real_input));
 }
 
-fn part1(input: &[String]) -> i64 {
-    let (grid, grid_size) = parse_input(input);
-    unsafe {
-        best_path = i64::MAX;
-    }
-    for (nx, ny) in find_neighbours(0, 0, grid_size) {
-        println!("from start, going into {},{}", nx, ny);
-        let mut visited = vec![(0, 0)];
-        enter(nx, ny, &grid, grid_size, &mut visited);
-    }
-    unsafe {
-        return best_path;
-    }
+fn part1(input: &[String]) -> i32 {
+    let mut cave = parse_input(input);
+    cave.find_least_risky_path()
 }
 
-fn part2(input: &[String]) -> i64 {
+fn part2(input: &[String]) -> i32 {
     0
 }
 
-fn enter(x: usize, y: usize, grid: &Vec<Vec<i8>>, grid_size: usize, visited: &mut Vec<(usize, usize)>) {
-    visited.push((x, y));
-    
-    unsafe {
-
-        let current_path_risk = calc_risk(visited, grid);
-        if current_path_risk < best_path {
-            if x == grid_size-1 && y == grid_size-1 {
-                best_path = current_path_risk;
-            } else {
-                let mut neighbours = find_neighbours(x, y, grid_size);
-
-                // remove all neighbours we've already visited
-                neighbours.retain(|p| !visited.contains(p));
-
-                for (nx, ny) in neighbours {
-                    enter(nx, ny, grid, grid_size, visited);
-                }
-            }
-        }
-
-    }
-
-    visited.pop();
-}
-
-fn calc_risk(path: &Vec<(usize, usize)>, grid: &Vec<Vec<i8>>) -> i64 {
-    path.iter().fold(0, |acc, (x, y)| grid[*y][*x] as i64 + acc) - (grid[0][0] as i64)
-}
-
-fn find_neighbours(x: usize, y: usize, grid_size: usize) -> Vec<(usize, usize)> {
-    let mut neighbours: Vec<(usize, usize)> = Vec::new();
-
-    for vector in NEIGHBOURS {
-        let nx = x as i8 + vector.0;
-        let ny = y as i8 + vector.1;
-        if nx >= 0 && nx < grid_size as i8 && ny >= 0 && ny < grid_size as i8 {
-            neighbours.push((nx as usize, ny as usize));
-        }
-    }
-
-    neighbours
-}
-
-fn print_grid(grid: &Vec<Vec<i8>>) {
-    for line in grid {
-        println!("{}", line.iter().map(|c| c.to_string()).collect::<Vec<String>>().join(""));
-    }
-}
-
-fn parse_input(input: &[String]) -> (Vec<Vec<i8>>, usize) {
+fn parse_input(input: &[String]) -> Cave {
     let mut grid: Vec<Vec<i8>> = Vec::new();
     let mut grid_size = 0;
     for (y, line) in input.iter().enumerate() {
-        if y == 0 { grid_size = line.len(); }
-        let mut row = vec![0; grid_size];
-        for (x, c) in line.chars().enumerate() {
-            row[x] = c.to_digit(10).unwrap() as i8;
+        if y == 0 { 
+            grid_size = line.len();
         }
+        let mut row = vec![0; grid_size];
+        line.chars().enumerate().for_each(|(x, c)| row[x] = c.to_digit(10).unwrap() as i8);
         grid.push(row);
     }
-    //print_grid(&grid);
-    (grid, grid_size)
+    Cave::new(grid, grid_size)
+}
+
+#[derive(Debug)]
+struct Cave {
+    chiton_risks: Vec<Vec<i8>>,
+    grid_size: usize,
+    lowest_risk_score: i32
+}
+
+impl Cave {
+
+    fn new(chiton_risks: Vec<Vec<i8>>, grid_size: usize) -> Cave {
+        Cave {
+            chiton_risks: chiton_risks,
+            grid_size: grid_size,
+            lowest_risk_score: i32::MAX
+        }        
+    }
+
+    fn print_grid(&self) {
+        self.chiton_risks
+            .iter()
+            .for_each(|row| println!("{}", row.iter().map(|risk| risk.to_string()).collect::<Vec<String>>().join("")));
+    }
+
+    fn find_neighbours(&self, x: usize, y: usize) -> Vec<(usize, usize)> {
+        let mut neighbours: Vec<(usize, usize)> = Vec::new();
+        for vector in NEIGHBOURS {
+            let nx = x as i8 + vector.0;
+            let ny = y as i8 + vector.1;
+            if nx >= 0 && nx < self.grid_size as i8 && ny >= 0 && ny < self.grid_size as i8 {
+                neighbours.push((nx as usize, ny as usize));
+            }
+        }
+        neighbours
+    }
+
+    fn calc_path_risk(&self, path: &Vec<(usize, usize)>) -> i32 {
+        path.iter().fold(0, |acc, (x, y)| self.chiton_risks[*y][*x] as i32 + acc)
+    }
+    
+    fn find_least_risky_path(&mut self) -> i32 {
+        for (nx, ny) in self.find_neighbours(0, 0) {
+            println!("from start, going into {},{}", nx, ny);
+            let mut visited = vec![(0, 0)];
+            self.enter(nx, ny, &mut visited);
+        }
+        self.lowest_risk_score
+    }
+
+    fn enter(&mut self, x: usize, y: usize, visited: &mut Vec<(usize, usize)>) {
+        let current_path_risk = self.calc_path_risk(visited);
+        if current_path_risk >= self.lowest_risk_score {
+            return;
+        }
+
+        visited.push((x, y));
+
+        if self.is_end(x, y) {
+            self.lowest_risk_score = current_path_risk;
+        } else {
+            let mut neighbours = self.find_neighbours(x, y);
+
+            // remove all neighbours we've already visited
+            neighbours.retain(|p| !visited.contains(p));
+
+            // sort be risk to visit lowest first
+            let mut neighbour_risks = neighbours
+                .iter()
+                .map(|p| (*p, self.chiton_risks[y][x])).collect::<Vec<((usize, usize), i8)>>();
+            neighbour_risks.sort_by(|(l_p, l_risk), (r_p, r_risk)| l_risk.cmp(r_risk));
+
+            for (nx, ny) in neighbours {
+                self.enter(nx, ny, visited);
+            }
+        }
+
+        visited.pop();
+    }
+
+    fn is_end(&self, x: usize, y: usize) -> bool {
+        (self.grid_size - 1, self.grid_size - 1) == (x, y)
+    }
+
 }
