e9659043588423c04e6ce0427a3c59e080d87e3e
Shaun Kenny <skenny@gmail.com>
2021-12-20 12:16:19 -0400

    day 20 tweaks...

--- day20.rs
+++ day20.rs
@@ -6,27 +6,68 @@ pub fn run() {
     let sample_input = aoc::read_input("input/day20-sample.txt");
     part1(&sample_input);
 
-    //let real_input = aoc::read_input("input/day20.txt");
-    //part1(&real_input);
+    let real_input = aoc::read_input("input/day20.txt");
+    part1(&real_input);
 }
 
 fn part1(input: &[String]) {
-    let (_enhancement_algo, mut image) = parse_input(input);
-    println!("part 1 = {}", 0);
-}
+    let (enhancement_algo, mut image) = parse_input(input);
 
-fn expand(vec: &mut Vec<Vec<bool>>) {
-    let original_height = vec.len();
-    vec.insert(0, vec![false; original_height + 2]);
-    vec.push(vec![false; original_height + 2]);
-    for row in 1..=original_height {
-        vec[row].insert(0, false);
-        vec[row].push(false)
+    println!("Input image is {:?}...", (image.len(), image.len()));
+    print_image(&image);
+
+    for _iteration in 0..2 {
+        let mut new_image = vec![vec![false; image.len() + 2]; image.len() + 2];
+        let mut infinite_value = false;
+        for y in 0..new_image.len() {
+            for x in 0..new_image.len() {
+                let orig_x = x as isize - 1;
+                let orig_y = y as isize - 1;
+                let pixels = find_pixels(orig_x, orig_y, &image, infinite_value);
+                let binary = pixels.iter().map(|pixel| if *pixel { "1" } else { "0" }).collect::<Vec<&str>>().join("");
+                let decimal = usize::from_str_radix(&binary, 2).unwrap();
+                let enhancement_pixel = enhancement_algo[decimal];
+                if orig_x < 0 && orig_y < 0 {
+                    infinite_value = enhancement_pixel;
+                }
+                new_image[y][x] = enhancement_pixel;
+            }
+        }
+        image = new_image;
     }
+
+    println!("Output image is {:?}...", (image.len(), image.len()));
+    print_image(&image);
+
+    let mut lit_pixels = 0;
+    for y in 0..image.len() {
+        for x in 0..image.len() {
+            if image[y][x] { lit_pixels += 1; }
+        }
+    }
+
+    println!("part 1 = {}", lit_pixels);
 }
 
-fn parse_input(input: &[String]) -> (String, Vec<Vec<bool>>) {
-    let enhancement_algo = input[0].to_string();
+fn find_pixels(x: isize, y: isize, image: &Vec<Vec<bool>>, infinite_value: bool) -> Vec<bool> {
+    let mut pixels: Vec<bool> = Vec::new();
+    let image_size = image.len() as isize;
+    for vy in -1..=1 {
+        for vx in -1..=1 {
+            let nx = x + vx;
+            let ny = y + vy;
+            if nx >= 0 && nx < image_size && ny >= 0 && ny < image_size {
+                pixels.push(image[ny as usize][nx as usize]);
+            } else {
+                pixels.push(infinite_value);
+            }
+        }
+    }
+    pixels
+}
+
+fn parse_input(input: &[String]) -> (Vec<bool>, Vec<Vec<bool>>) {
+    let enhancement_algo = input[0].chars().map(|c| if c == '.' { false } else { true }).collect();
     let mut image: Vec<Vec<bool>> = Vec::new();
     for line in 2..input.len() {
         image.push(input[line].chars().map(|c| if c == '.' { false } else { true }).collect());
